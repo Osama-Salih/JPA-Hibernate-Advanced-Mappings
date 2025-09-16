141fcb0f812c5b87a681006c7b4c5fea8497a4d8
Osama Salih <osamasalih.ca@gmail.com>
2025-09-16 17:35:56 +0300

    Develop the Main App

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.AppDAO;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +16,32 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner(String[] args) {
+	CommandLineRunner commandLineRunner(AppDAO appDao) {
 		return runner -> {
-			System.out.println("Hello world");
+			createInstructor(appDao);
 		};
+	}
+
+	private void createInstructor(AppDAO appDao) {
+//		Instructor instructor =
+//				new Instructor("Osama", "Salih", "os@luv2code.com");
+//
+//		InstructorDetail instructorDetail = new InstructorDetail(
+//				"https://www.luv2code.youtube",
+//				"luv 2 code"
+//		);
+//
+		Instructor instructor =
+				new Instructor("Madhu", "Patel", "madu@luv2code.com");
+
+		InstructorDetail instructorDetail = new InstructorDetail(
+				"https://www.luv2code.youtube",
+				"Guitar"
+		);
+
+		instructor.setInstructorDetail(instructorDetail);
+
+		System.out.println("Saving instructor");
+		appDao.save(instructor);
 	}
 }
