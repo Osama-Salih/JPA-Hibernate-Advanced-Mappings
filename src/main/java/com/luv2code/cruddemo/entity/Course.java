d942ddaf5bcf35dc21397d4f5946a0aab59d9813
Osama Salih <osamasalih.ca@gmail.com>
2025-09-20 16:14:10 +0300

    Uni-Directional - refactor course entity

--- src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -2,6 +2,9 @@ package com.luv2code.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "course")
 public class Course {
@@ -22,6 +25,10 @@ public class Course {
     })
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     public Course() {}
 
@@ -51,6 +58,22 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public void add(Review review) {
+        if (reviews == null) {
+            this.reviews = new ArrayList<>();
+        }
+
+        this.reviews.add(review);
     }
 
     @Override
